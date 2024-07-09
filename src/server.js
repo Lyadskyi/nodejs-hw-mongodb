5a215383d832250dca3c69c32a4994ef22a9faa4
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-07-09 23:58:44 +0300

    Added npms in the project

--- src/server.js
+++ src/server.js
@@ -1,61 +1,23 @@
-// import express from 'express';
-// import cors from 'cors';
+import express from 'express';
+import contacts from './services/contacts.js';
 
-// import movies from './movies.js';
+const app = express();
 
-// const app = express();
+const PORT = 3000;
 
-// app.use(cors());
+app.get('/', (req, res) => {
+  res.send('<h1>Home page</h1>');
+});
 
-// // const corsMiddleware = cors();
-// // app.use(corsMiddleware);
+app.get('/api/products', (req, res) => {
+  res.json([]);
+});
 
-// /*
-// const cors = options => {
-//     const middelware = (req, res, next)=> {
-//         // apply options
-//         res.setHeader('Access-Control-Allow-Origin', '*');
-//         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//         res.setHeader('Access-Control-Allow-Credentials', true);
-//         next();
-//     }
+app.get('/api/contacts', (req, res) => {
+  // const dataBaseResponse = null;
+  // res.send(dataBaseResponse);
+  // res.json(dataBaseResponse);
+  res.json(contacts);
+});
 
-//     return middleware;
-// }
-// */
-
-// // app.use((req, res, next) => {
-// //     res.setHeader('Access-Control-Allow-Origin', '*');
-// //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-// //     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-// //     res.setHeader('Access-Control-Allow-Credentials', true);
-// //     next();
-// // })
-
-// // app.use((req, res, next)=> {
-// //     console.log("First middleware");
-// //     next();
-// // })
-
-// // app.use((req, res, next)=> {
-// //     console.log("Second middleware");
-// //     next();
-// // })
-
-// app.get('/api/products', (req, res) => {
-//   res.json([]);
-// });
-
-// app.get('/api/movies', (req, res) => {
-//   res.json(movies);
-// });
-
-// app.use((req, res) => {
-//   res.status(404).json({
-//     status: 404,
-//     message: 'Not Found',
-//   });
-// });
-
-// app.listen(3000, () => console.log('Server running on 3000 PORT'));
+app.listen(3000, () => console.log(`Server is running on port ${PORT}`));
