ffa6048ab9461f1e2f09645da2f04ee375e6dae9
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-09-20 00:46:00 +0300

    Created a model and a UserCollection in a src\db\models\User.js
    Completed the first part of the second step

---
+++ src/db/models/User.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+
+import { email } from "../../constants/users.js";
+
+import { handleSaveError, setUpdateOptions } from "./hooks.js";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      match: email,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+userSchema.post("save", handleSaveError);
+
+userSchema.pre("findOneAndUpdate", setUpdateOptions);
+
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+const UserCollection = model("user", userSchema);
+
+export default UserCollection;
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import notFoundHandler from "./middlewares/notFoundHandler.js";
 import errorHandler from "./middlewares/errorHandler.js";
 // import logger from "./middlewares/logger.js";
 
+import authRouter from "./routers/auth.js";
 import contactsRouter from "./routers/contacts.js";
 
 export const setupServer = () => {
@@ -19,6 +20,7 @@ export const setupServer = () => {
   app.use(express.json());
 
   // Routes
+  app.use("/auth", authRouter);
   app.use("/contacts", contactsRouter);
 
   app.use(notFoundHandler);
